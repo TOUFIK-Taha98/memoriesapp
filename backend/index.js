ac72da4149f1ffefe4c8d3da2694b4955446710b
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-08-18 15:39:04 +0200

    using dotenv & cleaning code

--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,21 @@ import express from 'express';
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js'
 
 const app = express();
+dotenv.config();
 
+const urlDB = process.env.CONNEXION_URL
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json({limit: "30mb", extented:true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extented:true}));
 app.use(cors());
 
 app.use('/posts', postRoutes);
-
-const urlDB = "mongodb+srv://taha:taha123@cluster0.osxrutg.mongodb.net/memories?retryWrites=true&w=majority"
-const PORT = process.env.PORT || 5000;
 
 mongoose.connect(urlDB,{useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => app.listen(PORT, ()=> console.log('Connected to mongoose and listenning on 5000')))
