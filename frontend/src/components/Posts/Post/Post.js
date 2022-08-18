e7cd2889cdb1e85a0413785e4a0e1bd795f9460c
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-08-18 14:54:34 +0200

    Delete post (backend + db)

--- frontend/src/components/Posts/Post/Post.js
+++ frontend/src/components/Posts/Post/Post.js
@@ -36,7 +36,9 @@ const Post = ({post, setCurrentId}) => {
                     <ThumbUpAltIcon fontSize="small"/>
                     Like {post.likeCount}
                 </Button>
-                <Button size="small" color="primary" onClick={()=> {}}>
+                <Button size="small" color="primary" onClick={()=> {
+                    
+                }}>
                     <DeleteIcon fontSize="small"/>
                     Delete
                 </Button>
