47436e85318a559b80a96b679673cb68cd5501d4
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-08-18 16:05:48 +0200

    Changing the api url from localhost to heroku after the deployment

--- frontend/src/actions/posts.js
+++ frontend/src/actions/posts.js
@@ -1,41 +1,41 @@
-import * as api from '../api'
+import * as api from "../api";
 
-import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes'
-// Action Creators 
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from "../constants/actionTypes";
+// Action Creators
 
 export const getPosts = () => async (dispatch) => {
-    try {
-        const { data : posts } = await api.fetchPosts();
-        
-        dispatch({ type : FETCH_ALL, payload: posts});
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+  try {
+    const { data: posts } = await api.fetchPosts();
+
+    dispatch({ type: FETCH_ALL, payload: posts });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 export const createPost = (post) => async (dispatch) => {
-    try {
-        const { data : posts } = await api.createPost(post);
-        
-        dispatch({ type : CREATE, payload: posts});
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+  try {
+    const { data: posts } = await api.createPost(post);
+
+    dispatch({ type: CREATE, payload: posts });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 export const updatePost = (id, post) => async (dispatch) => {
-    try {
-        const {data} = await api.updatePost(id, post);
+  try {
+    const { data } = await api.updatePost(id, post);
 
-        dispatch({type: UPDATE, payload: data})
-    } catch (error) {
-        console.log(error)
-    }
-}
-export const deletePost = (id) => async(dispatch) => {
-    try {
-        await api.deletePost(id);
+    dispatch({ type: UPDATE, payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.deletePost(id);
 
-        dispatch({type: DELETE, payload: id})
-    } catch (error) {
-        console.log(error);
-    }
-}
+    dispatch({ type: DELETE, payload: id });
+  } catch (error) {
+    console.log(error);
+  }
+};
