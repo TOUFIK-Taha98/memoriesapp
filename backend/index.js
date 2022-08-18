792f6af7b0f6ae7a82132e812ffbb4823674544a
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-08-18 15:39:30 +0200

    Cleaning code

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,28 @@
-import express from 'express';
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
-import postRoutes from './routes/posts.js'
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 dotenv.config();
 
-const urlDB = process.env.CONNEXION_URL
+const urlDB = process.env.CONNEXION_URL;
 const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.json({limit: "30mb", extented:true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extented:true}));
+app.use(bodyParser.json({ limit: "30mb", extented: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extented: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes);
+app.use("/posts", postRoutes);
 
-mongoose.connect(urlDB,{useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => app.listen(PORT, ()=> console.log('Connected to mongoose and listenning on 5000')))
-.catch(err => console.log(err));
+mongoose
+  .connect(urlDB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log("Connected to mongoose and listenning on 5000")
+    )
+  )
+  .catch((err) => console.log(err));
